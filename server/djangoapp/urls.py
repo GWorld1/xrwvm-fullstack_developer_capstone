6f2dd69a7b7ad5eb8e509c99007ac330d6a46106
sans <sans@example.com>
2025-05-10 12:33:09 -0400

    module 4

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,7 +7,7 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     # path for registration
-    path(route='registration', view=views.registration, name='registration'),
+    path(route='register', view=views.registration, name='regis'),
 
     # path for login
      path(route='login', view=views.login_user, name='login'),
@@ -15,7 +15,7 @@ urlpatterns = [
      path(route='logout', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
-     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+     path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
      path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
      path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
     # path for add a review view
